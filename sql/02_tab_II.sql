08c32e387e299bb8965dd7f00f53771fac72ee68
przygoda <schmidt.multimedia@googlemail.com>
2023-12-13 11:02:43 +0000

    02 tab sql II ++

--- sql/02_tab_II.sql
+++ sql/02_tab_II.sql
@@ -1,18 +1,26 @@
-CREATE TABLE Countries (
+
+DROP TABLE IF EXISTS mydb.countries;
+
+CREATE TABLE mydb.countries (
   id INT NOT NULL AUTO_INCREMENT,
-  Country VARCHAR(100) NOT NULL,
-  Capital VARCHAR(100) NOT NULL,
-  Population INT NOT NULL,
+  country VARCHAR(100) NOT NULL,
+  capital VARCHAR(100) NOT NULL,
+  population INT NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Egypt', 'Cairo', 100000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Saudi Arabia', 'Riyadh', 34000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Iraq', 'Baghdad', 40000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Morocco', 'Rabat', 36000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Algeria', 'Algiers', 43000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Tunisia', 'Tunis', 12000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Jordan', 'Amman', 10000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Lebanon', 'Beirut', 7000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Oman', 'Muscat', 5000000);
-INSERT INTO Countries (Country, Capital, Population) VALUES ('Qatar', 'Doha', 3000000);
+DESCRIBE mydb.countries;
+
+INSERT INTO mydb.countries  (country, capital, population) VALUES 
+('Egypt', 'Cairo', 100000000),
+('Saudi Arabia', 'Riyadh', 34000000),
+('Iraq', 'Baghdad', 40000000),
+('Morocco', 'Rabat', 36000000),
+('Algeria', 'Algiers', 43000000),
+('Tunisia', 'Tunis', 12000000),
+('Jordan', 'Amman', 10000000),
+('Lebanon', 'Beirut', 7000000),
+('Oman', 'Muscat', 5000000),
+('Qatar', 'Doha', 3000000);
+
+SELECT * FROM mydb.countries;
